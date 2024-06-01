a3fde1575685b2389d3f2f569f0b7b3b1e253f57
Harish1611 <harishnaidub005@gmail.com>
2024-06-01 23:31:01 +0530

    Issue Fixed: JWT Middleware Secret Key issue resolved

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
-const config = require('../config');
+const dotenv = require('dotenv');
+dotenv.config();
 
+SECRET_KEY = process.env.SECRET_KEY;
 const authMiddleware = (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
   if (!token) {
@@ -8,7 +10,7 @@ const authMiddleware = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, config.jwtSecret);
+    const decoded = jwt.verify(token, SECRET_KEY);
     req.user = decoded;
     next();
   } catch (error) {
