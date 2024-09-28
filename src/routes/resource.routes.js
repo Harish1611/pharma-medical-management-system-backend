18011b3206f98acd4362b658317e214702a81633
Harish1611 <harishnaidub005@gmail.com>
2024-09-28 21:29:38 +0530

    New: resource GET end point configured with controller

--- src/routes/resource.routes.js
+++ src/routes/resource.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const resourceController = require('../controllers/resource.controller');
 
 router.post('/create-resource', resourceController.createResource);
+router.get('/get-resources', resourceController.getAllResources);
 
 module.exports = router;
