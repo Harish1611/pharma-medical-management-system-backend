1574f20d7f26a00418b1e5c4b35d00484c636537
Harish1611 <harishnaidub005@gmail.com>
2024-09-29 20:26:39 +0530

    New: GET endpoint with id as param is created in resource routes

--- src/routes/resource.routes.js
+++ src/routes/resource.routes.js
@@ -4,5 +4,6 @@ const resourceController = require('../controllers/resource.controller');
 
 router.post('/create-resource', resourceController.createResource);
 router.get('/get-resources', resourceController.getAllResources);
+router.get('/id/:id', resourceController.getResourceById);
 
 module.exports = router;
